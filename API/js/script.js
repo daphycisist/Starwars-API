42af13e30774a633837dbf5ec9c7b675c71d9c33
daphycisist <daphycisist@gmail.com>
2020-07-25 07:38:16 +0100

    fetch and append each user to index

--- API/js/script.js
+++ API/js/script.js
@@ -1,27 +1,32 @@
-const ul = document.getElementById('actors'); // Get the list where we will place our authors
-const url = 'https://swapi.dev/api/people/'; // Get 10 random users
+const div = document.getElementById('actors'); // Get the list where we will place our authors
+const url = 'https://swapi.dev/api/people/?results'; // Get 10 random users
 
 const createActors = actor => document.createElement(actor); // Create the type of element you pass in the parameters
 
 const appendActor = (parent, el) => parent.appendChild(el); // Append the second parameter(element) to the first one
 
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        let actors = data.results;
+        return actors.map((actor, index) => {
+            let imageUrl = `https://picsum.photos/200?=${index}`;
 
+            let image = createActors('img');
+            let user = createActors('div');
+            user.id = `${index + 1}`
+            let name = createActors('p');
+            image.src = imageUrl;
+            name.innerHTML = `${actor.name}`;
 
-fetch(url) // Transform the data into json
-    .then((resp) => resp.json())
-    .then(data => {
-        let actors = data.results; // Get the results
-        return actors.map(function (actor) { // Map through the results and for each run the code below
-            let li = createActors('li'), //  Create the elements we need
-                // img = createActors('img'),
-                span = createActors('span');
-            // img.src = author.picture.medium;  // Add the source of the image to be the src of the img element
-            span.innerHTML = `${actor.name}`; // Make the HTML of our span to be the first and last name of our author
-            // append(li, img); // Append all our elements
-            appendActor(li, span);
-            appendActor(ul, li);
+            appendActor(div, user);
+            appendActor(user, image);
+            appendActor(user, name);
         })
     })
+
+
 
 
 
