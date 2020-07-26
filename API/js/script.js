6bc2b5f56507d8e2529e59a78806295d8a11f112
daphycisist <daphycisist@gmail.com>
2020-07-26 16:02:27 +0100

    add grid content

--- API/js/script.js
+++ API/js/script.js
@@ -13,20 +13,23 @@ fetch(url)
     let actors = data.results; //Get all actors data
     
    
-    // console.log(user);
     return actors.map((actor, index) => {
       let actorsInfos = new User(actor)
-      // console.log(actorsInfos);
       let actorInfo = actorsInfos.actorDetails
       console.log(actorInfo);
       
       
-      let imageUrl = `https://picsum.photos/200?=${index}`; //Get random images for each actor's data
+      let imageUrl = ['./img/TheMandalorian-Logo.jpg', './img/stars-bcg.jpg', './img/masked-soldiers.jpg']
+
+      // let imageUrl = `https://picsum.photos/200?=${index}`; //Get random images for each actor's data
       let image = createActors("img");
       let person = createActors("div");
-      person.id = `${index + 1}`; //Give each person a unique id
+      person.classList.add('actor-detail')
+      // person.id = `${index + 1}`; //Give each person a unique id
       let name = createActors("p");
-      image.src = imageUrl;
+      name.classList.add('actor-name');
+      // name.style.cursor = 'pointer'
+      image.src = imageUrl[index];
       name.innerHTML = `${actor.name}`; //Populate our HTML with each person's name
 
       appendActor(div, person);
@@ -35,19 +38,19 @@ fetch(url)
 
       name.onclick = () => {
         let actor_content = document.querySelector('.actor-content')
-        actor_content.style.display = 'block';
-
-
         let article = document.querySelector('.display-actor-info')
+        actor_content.style.display = 'block';
         article.innerHTML = `
-          <p>${actorInfo.name}</p>
-          <p>${actorInfo.gender}</p>
-          <p>${actorInfo.height}</p>
+        <div class="actor-img">
           <img src='${imageUrl}'></img>
+        </div>
+        <div class="actor-info">
+          <p>Name: ${actorInfo.name}</p>
+          <p>Gender: ${actorInfo.gender}</p>
+          <p>Height: ${actorInfo.height}</p>
+        </div>
           <button class="close">&times;</button>
         `  
-        // let actor_content = document.querySelector('.actor_content')
-        // actor_content.style.display = 'block';
         let closeBtn = document.querySelector('.close')
         closeBtn.onclick = (e) => {
           actor_content.style.display = 'none'
