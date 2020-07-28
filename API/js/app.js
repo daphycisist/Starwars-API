c30267809f2977fb806b2f36fc022a5f8c81a149
daphycisist <daphycisist@gmail.com>
2020-07-28 23:10:37 +0100

    add footer media query

--- API/js/app.js
+++ API/js/app.js
@@ -1,38 +1,60 @@
+//import User class 
 import { User } from "../modules/User.js";
 
-let imageUrl = ['./img/Luke-Skywalker.jpg', './img/C-3PO.jpg', './img/R2-D2.jpg', './img/Darth-Vader.jpg', './img/Leia-Organa.png', './img/Owen-Lars.jpeg', './img/Beru-Lars.jpeg', './img/R5-D4.jpeg', './img/Biggs-Darklighter.jpeg', './img/Obi-Wan-Kenobi.jpeg', './img/Obi-Wan-Kenobi.jpeg'];
+//Static image paths to populate actor details 
+const imageUrl = [
+  './img/Luke-Skywalker.jpg',
+  './img/C-3PO.jpg', 
+  './img/R2-D2.jpg', 
+  './img/Darth-Vader.jpg', 
+  './img/Leia-Organa.png', 
+  './img/Owen-Lars.jpeg', 
+  './img/Beru-Lars.jpeg', 
+  './img/R5-D4.jpeg', 
+  './img/Biggs-Darklighter.jpeg', 
+  './img/Obi-Wan-Kenobi.jpeg',
+];
 
-const div = document.getElementById("actors"); // Get the div where we will place our authors
-const url = "https://swapi.dev/api/people/"; // API to fetch data from
+// API to fetch data from
+const url = "https://swapi.dev/api/people/"; 
 
+// Functions to create actor models 
 const createActors = (actor) => document.createElement(actor);
 const appendActor = (parent, el) => parent.appendChild(el);
 
+//Fetch Function
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    let actors = data.results; //Get all actors data
     
-   
+    //Get all actors data
+    const actors = data.results; 
+    
+    // Loop through the API and append data to index page
     return actors.map((actor, index) => {
-      let actorsInfos = new User(actor)
-      let actorInfo = actorsInfos.actorDetails
-      console.log(actorInfo);
-      let image = createActors("img");
-      let person = createActors("div");
-      person.classList.add('actor-detail')
-      let name = createActors("p");
+      const div = document.getElementById("actors"); 
+      const actorsInfos = new User(actor);
+      const actorInfo = actorsInfos.actorDetails;
+      const image = createActors("img");
+      const person = createActors("div");
+      const name = createActors("p");
+
+      person.classList.add('actor-detail');
       name.classList.add('actor-name');
       image.src = imageUrl[index];
-      name.innerHTML = `${actor.name}`; //Populate our HTML with each person's name
 
+      //Populate our HTML with each person's name
+      name.innerHTML = `${actor.name}`; 
+
+      
       appendActor(div, person);
       appendActor(person, image);
       appendActor(person, name);
-
+      
+      // Click function which populates the selected data into the modal
       name.onclick = () => {
-        let actor_content = document.querySelector('.actor-content')
-        let article = document.querySelector('.display-actor-info')
+        const actor_content = document.querySelector('.actor-content');
+        const article = document.querySelector('.display-actor-info');
         actor_content.style.display = 'block';
         article.innerHTML = `
         <div class="actor-img">
@@ -45,10 +67,14 @@ fetch(url)
         </div>
           <button class="close">&times;</button>
         `  
-        let closeBtn = document.querySelector('.close')
+
+        const closeBtn = document.querySelector('.close');
         closeBtn.onclick = () => {
           actor_content.style.display = 'none'
         } 
       }
     });
+  })
+  .catch(error => {
+    console.log(error);
   });
